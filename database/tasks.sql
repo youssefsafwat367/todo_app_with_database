fb6d97dad620de2953f1ae03fe134a7cff966b6e
youssefsafwat367 <141787232+youssefsafwat367@users.noreply.github.com>
2023-08-23 04:49:01 +0300

    Update tasks.sql

--- database/tasks.sql
+++ database/tasks.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `todo_app_g317`
+-- Database: `tasks`
 --
 
 -- --------------------------------------------------------
